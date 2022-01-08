d43da42d0220673b891ddf9e65662de2ed88b4bb
Runkai Zhang <Runkai Zhang>
2022-01-08 11:50:58 -0500

    Add headers to allow cross origin

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request))
 })
 
+const myHeaders = new Headers();
+myHeaders.set("Access-Control-Allow-Origin", '*');
+myHeaders.set("Access-Control-Allow-Methods", "GET,POST");
+myHeaders.set("Access-Control-Max-Age", "86400",);
+myHeaders.set('content-type', 'text/plain');
+
 async function handleRequest(request) {
   if (request.method === "POST") {
     return addPost(await request.text())
@@ -20,20 +26,18 @@ async function addPost(markdown) {
   const result = await BLOG.put(Date.now(), markdown)
 
   return new Response('Post Uploaded!', {
-    headers: { 'content-type': 'text/plain' },
+    headers: myHeaders,
   })
 }
 
 async function readPost(key) {
   const result = await BLOG.get(key)
 
-  return new Response(result, {
-    headers: { 'content-type': 'text/plain' }
-  })
+  return new Response(result, { status: 200, headers: myHeaders })
 }
 
 async function listPosts() {
   const result = await BLOG.list()
 
-  return new Response(JSON.stringify(result.keys))
+  return new Response(JSON.stringify(result.keys), { status: 200, headers: myHeaders })
 }
