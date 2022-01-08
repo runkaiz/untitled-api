65bff6baf09bf4cfd9dec3b405df5462d3c9530a
Runkai Zhang <Runkai Zhang>
2022-01-08 13:39:50 -0500

    Fine tune the headers

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request))
 })
 
-const myHeaders = new Headers();
-myHeaders.set("Access-Control-Allow-Origin", '*');
-myHeaders.set("Access-Control-Allow-Methods", "GET,POST");
-myHeaders.set("Access-Control-Max-Age", "86400",);
-myHeaders.set('content-type', 'text/plain');
-
 async function handleRequest(request) {
   if (request.method === "POST") {
     return addPost(await request.text())
@@ -23,6 +17,12 @@ async function handleRequest(request) {
 }
 
 async function addPost(markdown) {
+  const myHeaders = new Headers();
+  myHeaders.set("Access-Control-Allow-Origin", '*');
+  myHeaders.set("Access-Control-Allow-Methods", "POST");
+  myHeaders.set("Access-Control-Max-Age", "86400",);
+  myHeaders.set('content-type', 'text/plain');
+
   const result = await BLOG.put(Date.now(), markdown)
 
   return new Response('Post Uploaded!', {
@@ -31,12 +31,23 @@ async function addPost(markdown) {
 }
 
 async function readPost(key) {
+  const myHeaders = new Headers();
+  myHeaders.set("Access-Control-Allow-Origin", '*');
+  myHeaders.set("Access-Control-Allow-Methods", "GET");
+  myHeaders.set("Access-Control-Max-Age", "86400",);
+  myHeaders.set('content-type', 'text/plain');
+
   const result = await BLOG.get(key)
 
   return new Response(result, { status: 200, headers: myHeaders })
 }
 
 async function listPosts() {
+  const myHeaders = new Headers();
+  myHeaders.set("Access-Control-Allow-Origin", '*');
+  myHeaders.set("Access-Control-Allow-Methods", "GET");
+  myHeaders.set("Access-Control-Max-Age", "86400",);
+  myHeaders.set('content-type', 'application/json');
   const result = await BLOG.list()
 
   return new Response(JSON.stringify(result.keys), { status: 200, headers: myHeaders })
